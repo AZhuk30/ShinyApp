b31784572697338a90b2a118832e1a7112ad5d11
Alexander Zhuk <Azhuk30@gmail.com>
2025-11-23 14:23:08 -0800

    Update Setup_connect.R

--- Setup_connect.R
+++ Setup_connect.R
@@ -1,1 +1,62 @@
-rsconnect::writeManifest()
+{
+  "version": 1,
+  "locale": "en_US",
+  "metadata": {
+    "appmode": "shiny",
+    "entrypoint": "app.R",
+    "primary_rmd": null,
+    "primary_html": null,
+    "content_category": null,
+    "has_parameters": false
+  },
+  "python": null,
+  "quarto": null,
+  "files": {
+    "app.R": {
+      "checksum": "computed_by_rsconnect"
+    },
+    "AustralianWines.csv": {
+      "checksum": "computed_by_rsconnect"
+    }
+  },
+  "packages": {
+    "R": [
+      {
+        "package": "shiny",
+        "version": "latest",
+        "source": "CRAN"
+      },
+      {
+        "package": "tidyverse",
+        "version": "latest",
+        "source": "CRAN"
+      },
+      {
+        "package": "fpp3",
+        "version": "latest",
+        "source": "CRAN"
+      },
+      {
+        "package": "plotly",
+        "version": "latest",
+        "source": "CRAN"
+      },
+      {
+        "package": "DT",
+        "version": "latest",
+        "source": "CRAN"
+      },
+      {
+        "package": "bslib",
+        "version": "latest",
+        "source": "CRAN"
+      },
+      {
+        "package": "urca",
+        "version": "latest",
+        "source": "CRAN"
+      }
+    ]
+  },
+  "users": null
+}
